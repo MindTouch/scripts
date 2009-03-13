18f741902b9dfafb252062d97905aa9945621dbb
arnec <arnec@0eb84ffb-6e0e-0410-b475-cc1b69d517b7>
2009-03-13 21:51:08 +0000

    Deki (trunk): * added code to dekiapi to define a dummy console.log if undefined, so JEM can use console.log with impunity

--- res/deki/deki.api.js
+++ res/deki/deki.api.js
@@ -21,6 +21,9 @@
  * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
  * http://www.gnu.org/copyleft/gpl.html
  */
+if( typeof console == 'undefined' ) {
+	console = { log: function(str) {} };
+}
 
 if(typeof Deki.Api == 'undefined') {
 	Deki.Api = {
